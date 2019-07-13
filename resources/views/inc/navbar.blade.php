a7263c5abac948a3454e9dbca0037ae42e7c491a
Raywire <ryanwire@outlook.com>
2019-07-13 23:02:42 +0300

    Adds a form and saves data

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -4,19 +4,22 @@
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-        <li class="nav-item active">
-            <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="/about">About</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="/services">Services</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="/posts">Blogs</a>
-        </li>
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/about">About</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/services">Services</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/posts">Blogs</a>
+            </li>
         </ul>
+        <ul class="navbar-nav">
+            <li><a href="/posts/create">Create Post</a></li>
+        <ul>
     </div>
 </nav>
---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <form action="/posts" enctype="multipart/form-data" method="post">
+  @csrf
+
+    <div class="row">
+      <div class="col-8 offset-2">
+      <div class="row"><h1>Add New Post</h1></div>
+        <div class="form-group row">
+          <label for="title" class="col-form-label">Title</label>
+          <input id="title" name="title" type="text" class="form-control @error('title') is-invalid @enderror" title="title" value="{{ old('title') }}" placeholder="Title"  autocomplete="title" autofocus>
+
+          @error('title')
+            <span class="invalid-feedback" role="alert">
+              <strong>{{ $message }}</strong>
+            </span>
+          @enderror
+        </div>
+        <div class="form-group row">
+            {{-- <label for="body" class="col-form-label">Body</label> --}}
+            <textarea id="article-ckeditor" name="body" class="form-control @error('body') is-invalid @enderror" body="body">
+            </textarea>
+            @error('body')
+                <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+        </div>
+        <div class="row pt-4">
+          <button class="btn btn-primary">Add New Post</button>
+        </div>
+      </div>
+    </div>
+  </form>
+</div>
+@endsection
